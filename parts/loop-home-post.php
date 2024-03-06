8996f87b456ef02ec50876adb9d4115e0b5e768d
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-03-06 16:33:32 +0200

    Redesign post preview in home page loop.

--- parts/loop-home-post.php
+++ parts/loop-home-post.php
@@ -1,27 +1,31 @@
 <!-- BEGIN of Post -->
-<article id="post-<?php the_ID(); ?>" <?php post_class('preview preview--' . get_post_type()); ?>>
-    <div class="grid-x grid-margin-x">
-        <?php if (has_post_thumbnail()) : ?>
-            <div class="medium-4 small-12 cell text-center medium-text-left">
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                    <?php the_post_thumbnail('medium', array('class' => 'preview__thumb')); ?>
-                </a>
+<article id="post-<?php the_ID(); ?>" <?php post_class('cell post-block preview preview--' . get_post_type()); ?>>
+    <div class="grid-x grid-margin-x grid-margin-y">
+        <div class="cell">
+            <div class="preview__meta grid-x align-justify">
+                <span><?php echo get_the_time('d.m'); ?></span>
+                <span><?php echo get_the_time('Y'); ?></span>
             </div>
-        <?php endif; ?>
-        <div class="cell auto">
-            <h3 class="preview__title">
+        </div>
+        <div class="cell">
+            <h5 class="preview__title">
                 <a href="<?php the_permalink(); ?>"
                    title="<?php echo esc_attr(sprintf(__('Permalink to %s', 'fxy'), the_title_attribute('echo=0'))); ?>"
                    rel="bookmark"><?php echo get_the_title() ?: __('No title', 'fxy'); ?>
                 </a>
-            </h3>
-            <?php if (is_sticky()) : ?>
-                <span class="secondary label preview__sticky"><?php _e('Sticky', 'fxy'); ?></span>
-            <?php endif; ?>
-            <div class="preview__excerpt">
-                <?php the_excerpt(); // Use wp_trim_words() instead if you need specific number of words?>
+            </h5>
+        </div>
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="cell">
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php the_post_thumbnail('medium_large', array('class' => 'preview__thumb')); ?>
+                </a>
             </div>
-            <p class="preview__meta"><?php echo sprintf(__('Written by %s on %s', 'fxy'), get_the_author_posts_link(), get_the_time(get_option('date_format'))); ?></p>
+        <?php endif; ?>
+        <div class="cell">
+            <p class="preview__excerpt">
+                <?php echo wp_trim_words(get_the_excerpt(), 20);?>
+            </p>
         </div>
     </div>
 </article>
