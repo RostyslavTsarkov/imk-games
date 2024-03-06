3040849a81a0cfce1997cd1b855b1992aa99933d
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-03-06 16:26:46 +0200

    Update search form layout and refactor UI component styles
    Significantly altered the styles for padding, font sizes, and colors. Improved search form layout and added magnifying glass icon to the search button.

--- searchform.php
+++ searchform.php
@@ -6,8 +6,12 @@
  */
 ?>
 <!-- BEGIN of search form -->
-<form method="get" class="search" action="<?php echo esc_url(home_url('/')); ?>">
-    <input type="search" name="s" class="search__input" placeholder="<?php _e('Search', 'fxy'); ?>" value="<?php echo get_search_query(); ?>" aria-label="<?php _e('Search input', 'fxy'); ?>"/>
-    <button type="submit" name="submit" class="search__submit" aria-label="<?php _e('Submit search', 'fxy'); ?>"><?php _e('Search', 'fxy'); ?></button>
-</form>
+<div class="search-container">
+    <form method="get" class="search" action="<?php echo esc_url(home_url('/')); ?>">
+        <input type="search" name="s" class="search__input" placeholder="<?php _e('Search', 'fxy'); ?>" value="<?php echo get_search_query(); ?>" aria-label="<?php _e('Search input', 'fxy'); ?>"/>
+        <button type="submit" name="submit" class="search__submit" aria-label="<?php _e('Submit search', 'fxy'); ?>">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+    </form>
+</div>
 <!-- END of search form -->
