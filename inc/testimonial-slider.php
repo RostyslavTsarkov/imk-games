fe30733a7592cecab5f1674acab32f9d67bdbdd2
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-03-21 10:40:38 +0200

    Changed the way images is displayed.

--- inc/testimonial-slider.php
+++ inc/testimonial-slider.php
@@ -28,49 +28,41 @@ add_shortcode('testimonial-slider', function () {
         });
     </script>
 
-    <?php
-    if ($testimonials = get_field('home_featured_testimonials')) : ?>
-        <div id="testimonial-slider" class="slick-slider home-slider testimonial-slider">
-            <?php if ($border = get_field('testimonial_slider_border_image')) : ?>
-                <?php echo wp_get_attachment_image($border, false, false, array('class' => 'testimonial-slider__border-img')); ?>
-            <?php endif; ?>
-            <?php if ($substrate = get_field('testimonial_slider_substrate')) : ?>
-                <?php echo wp_get_attachment_image($substrate, false, false, array('class' => 'testimonial-slider__substrate')); ?>
-            <?php endif; ?>
-            <?php foreach ($testimonials as $post) :
-                setup_postdata($post); ?>
-                <div class="slick-slide home-slide">
-                    <?php if ($avatar = get_post_thumbnail_id($post->ID)) : ?>
-                        <?php echo wp_get_attachment_image($avatar, 'medium', false, array('class' => 'testimonial-slider__avatar')); ?>
-                    <?php endif; ?>
-                    <div class="home-slide__inner">
-                        <div class="grid-container home-slide__caption">
-                            <div class="grid-x grid-margin-x">
-                                <div class="cell text-center">
-                                    <div class="grid-y row-gap-60 row-gap-25-medium align-justify align-middle">
-                                        <h3>
-                                            <?php echo get_the_title($post->ID); ?>
-                                        </h3>
-                                        <div class="testimonial-slider__decor"></div>
-                                        <?php the_content(); ?>
-                                    </div>
+    <div id="testimonial-slider" class="slick-slider home-slider testimonial-slider">
+        <img class="testimonial-slider__border-img" src="<?php echo asset_path('images/decoration/testimonials-border.png') ?>">
+        <img class="testimonial-slider__substrate" src="<?php echo asset_path('images/decoration/testimonials-substrate.png') ?>">
+        <?php foreach (get_field('home_featured_testimonials') as $post) :
+            setup_postdata($post); ?>
+            <div class="slick-slide home-slide">
+                <?php if ($avatar = get_post_thumbnail_id($post->ID)) : ?>
+                    <?php echo wp_get_attachment_image($avatar, 'medium', false, array('class' => 'testimonial-slider__avatar')); ?>
+                <?php endif; ?>
+                <div class="home-slide__inner">
+                    <div class="grid-container home-slide__caption">
+                        <div class="grid-x grid-margin-x">
+                            <div class="cell text-center">
+                                <div class="grid-y row-gap-60 row-gap-25-medium align-justify align-middle">
+                                    <h3>
+                                        <?php echo get_the_title($post->ID); ?>
+                                    </h3>
+                                    <div class="testimonial-slider__decor"></div>
+                                    <?php the_content(); ?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            <?php endforeach; ?>
-            <?php
-            wp_reset_postdata();
-            if ($link = get_field('home_testimonials_archive_link')) : ?>
-                <a class="testimonial-slider__button button"
-                   href="<?php echo esc_url($link['url']); ?>">
-                    <?php echo $link['title']; ?>
-                </a>
-            <?php endif; ?>
-        </div><!-- END of  #testimonial-slider-->
+            </div>
+        <?php endforeach; ?>
         <?php
-    endif;
-
+        wp_reset_postdata();
+        if ($link = get_field('home_testimonials_archive_link')) : ?>
+            <a class="testimonial-slider__button button"
+               href="<?php echo esc_url($link['url']); ?>">
+                <?php echo $link['title']; ?>
+            </a>
+        <?php endif; ?>
+    </div><!-- END of  #testimonial-slider-->
+    <?php
     return ob_get_clean();
 });
